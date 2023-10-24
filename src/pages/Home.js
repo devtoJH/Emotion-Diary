bc050f7910f8174bb31385752ee2afef5952391b
JIHYUN <qkrwlgus718@gmail.com>
2023-10-24 21:34:47 +0900

    감정 일기장 앱 생성

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import Button from "../component/Button";
+
+const Home = () => {
+  return (
+    <div>
+      <Button text={"버튼 텍스트"} onClick={() => { alert('hi'); }} />
+    </div>
+  )
+};
+
+export default Home;
