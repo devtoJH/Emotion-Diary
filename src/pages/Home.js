335f4a226dd5599358df08be28dca36df8681b91
JIHYUN <qkrwlgus718@gmail.com>
2023-10-30 21:20:21 +0900

    Editor 컴포넌트 생성1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,18 +1,13 @@
 import Button from "../component/Button";
+import Editor from "../component/Editor";
 import Header from "../component/Header";
 
 const Home = () => {
   return (
     <div>
-      <Header 
-        title={"Home"} 
-        leftChild={
-          <Button type="positive" text={"긍정 버튼"} onClick={() => { alert('positive button'); }} />
-        }
-        rightChild={
-          <Button type="negative" text={"부정 버튼"} onClick={() => { alert('negative button'); }} />
-        }
-      />
+      <Editor onSubmit={() => {
+        alert("작성 완료 버튼을 클릭했음");
+      }} />
     </div>
   );
 };
