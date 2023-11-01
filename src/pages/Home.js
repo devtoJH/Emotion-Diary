47c562821d854eb39bc6fe1a7c5e34631c776158
JIHYUN <qkrwlgus718@gmail.com>
2023-11-01 21:33:13 +0900

    일기 관리 기능 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,14 +5,7 @@ import Header from "../component/Header";
 const Home = () => {
   return (
     <div>
-      <Editor 
-        initData={{
-          date: new Date().getTime(),
-          emotionId:1,
-          content: "이전에 작성했던 일기",
-        }}
-        onSubmit={() => alert("작성 완료!")}
-      />
+      Home 페이지
     </div>
   );
 };
