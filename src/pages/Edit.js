6b842fd74d6b1d3ce6daf34c06d1b9c35f975a77
JIHYUN <qkrwlgus718@gmail.com>
2023-11-13 19:19:52 +0900

    Edit 페이지_일기 데이터 가져오기

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,5 +1,29 @@
+import { useParams, useNavigate } from "react-router-dom";
+import useDiary from "../hooks/useDiary";
+import Button from "../component/Button";
+import Header from "../component/Header";
+
 const Edit = () => {
-  return <div>Edit 페이지 입니다.</div>
+  const {id} = useParams();
+  const data = useDiary(id);
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
+  if (!data) {
+    return <div>일기를 불러오고 있습니다...</div>
+  } else {
+    return (
+      <div>
+        <Header
+          title={"일기 수정하기"}
+          leftChild={<Button text={"< 뒤로가기"} onClick={goBack} />}
+          rightChild={<Button type={"nagative"} text={"삭제하기"} />}
+        />
+      </div>
+    );
+  }
 };
 
 export default Edit;
